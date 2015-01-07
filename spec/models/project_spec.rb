9f87c01df88a92968d0e9af88be4bbd2c7ef4c26
Erik Olsen <erik.s.olsen@gmail.com>
2015-01-07 17:43:05 -0600

    Project is done when all tasks are completed

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Project do
     expect(project).not_to be_done
   end
 
+  it "marks a project done if its tasks are done" do 
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
+  end
+  
 end
