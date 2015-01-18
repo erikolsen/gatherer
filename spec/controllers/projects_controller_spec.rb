3aaaa689dc505a4a14bf7420e3f70ea37497446f
Erik Olsen <erik.s.olsen@gmail.com>
2015-01-18 14:07:53 -0600

    Create project passes feature test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe ProjectsController, :type => :controller do
   describe 'POST create' do
     it 'creates a project' do
       post :create, project: {name: 'Runway', tasks: 'Start Something:2'}
-      expect(response).to redirect_to_(projects_path)
+      expect(response).to redirect_to(projects_path)
       expect(assigns(:action).project.name).to eq('Runway')
+    end
+
+    it 'goes back to the form on failure' do
+      post :create, project: { name: '', tasks: '' }
+      expect(response).to render_template(:new)
+      expect(assigns(:project)).to be_present
     end
   end
 
